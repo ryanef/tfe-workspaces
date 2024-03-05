fb389b9300c959c494d8f9ff30c9eb060d76a6fa
ryanef <ryanef39@gmail.com>
2024-03-05 10:41:29 -0500

    Vpc (#3)
    * locals
    
    * add vpc

--- locals.tf
+++ locals.tf
@@ -12,23 +12,25 @@ locals {
       
         tfe-workspaces  = {
             description = "automate tfe workspaces"
-            execution_mode = "local"
+            execution_mode = "remote"
             organization_name = var.organization_name
             project_id = module.project["controller"].project_id
             vcs_repo_identifier = "${var.github_organization_name}/tfe-workspaces"
         }
 
-
-
-
         tfe-github-automate = {
-
             description = "Automation workspace for GitHub Repos"
-
             execution_mode = "remote"
-
             project_id = module.project["controller"].project_id
             vcs_repo_identifier = "${var.github_organization_name}/tfe-github-automate"
+            organization_name = var.organization_name
+
+          }
+        tfe-aws-vpc-automate = {
+            description = "Automation workspace for AWS VPCs"
+            execution_mode = "remote"
+            project_id = module.project["controller"].project_id
+            vcs_repo_identifier = "${var.github_organization_name}/tfe-aws-vpc-automate"
             organization_name = var.organization_name
 
           }
