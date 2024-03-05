11c6843b187fc947249222e0c209166c719fdb67
ryanef <ryanef39@gmail.com>
2024-03-05 10:05:30 -0500

    execution mode to remote

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
       
         tfe-workspaces  = {
             description = "automate tfe workspaces"
-            execution_mode = "local"
+            execution_mode = "remote"
             organization_name = var.organization_name
             project_id = module.project["controller"].project_id
             vcs_repo_identifier = "${var.github_organization_name}/tfe-workspaces"
@@ -24,7 +24,7 @@ locals {
         tfe-github-automate = {
 
             description = "Automation workspace for GitHub Repos"
-            execution_mode = "local"
+            execution_mode = "remote"
             project_id = module.project["controller"].project_id
             vcs_repo_identifier = "${var.github_organization_name}/tfe-github-automate"
             organization_name = var.organization_name
